c0643eb177128c10856b0c1c84c684c9c66d605b
Sophia Solorzano <Sophia.E.Solorzano-1@ou.edu>
2025-09-10 11:23:08 -0500

    Console Added

--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1b Grade
 
-// Your Name:
+// Your Name: Sophia Solorzano
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,43 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            // Prompt for student info
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+            string studentId = Console.ReadLine();
+
+            // Prompt for grades
+            Console.WriteLine("What is your overall percentage grade for assignmentss?");
+            double assignments = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            double participation = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade examone?");
+            double examone = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for examtwo?");
+            double examtwo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for examthree?");
+            double examthree = Convert.ToDouble(Console.ReadLine());
+
+            // Weighting from syllabus
+            double finalGrade = (assignments * 0.20) +
+                                (participation * 0.15) +
+                                (examone * 0.15) +
+                                (examtwo * 0.25) +
+                                (examthree * 0.25);
+
+            // Output result
+            Console.WriteLine($"{firstName} {lastName} ({studentId}), your final grade is {finalGrade:F2}%");
+
+            Console.ReadKey();
         }
     }
 }
